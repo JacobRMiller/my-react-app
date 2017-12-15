72c1c4e57d6e1e25155ad250e1a8de17fe814599
mesync <mesync@gmail.com>
2017-12-15 17:35:47 -0500

    Merge 1 from Jakes Mac

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -12,9 +12,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.<br>
+          To get started, edit <code>src/App.js</code> and save to reload.
          <Greeting/>
-          </br>
         </p>
       </div>
     );
