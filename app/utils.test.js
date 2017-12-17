2e22198957f797ac8b3eff2d5a88733ca80cf562
mesync <mesync@gmail.com>
2017-12-17 11:52:05 -0500

    Updating sample test.

--- app/utils.test.js
+++ app/utils.test.js
@@ -1,10 +1,9 @@
-require ('./utils.js');
-
 const utils = require('./utils.js');
 
 it("should add two numbers", () => {
-    var r = utils.add(2,1);
+    var r = utils.add(20,80);
 
-    if (r != 4) {
-        throw new Error('Value not expected 4 but got ${r}.');    }
+    if (r != 100) {
+        throw new Error(`Value not expected 4 but got ${r}.`);  
+     }
 });
