916cf4db859c92e4fc6f3ea3e9f3f8a009078ec8
mesync <mesync@gmail.com>
2018-02-18 20:45:21 -0500

    adding updated files

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
 const mongoconnect = require('./app/mongodb-connect.js');
+const verifyToken = require('./auth/verifyToken.js');
 
 var app = express();
-var port = 3000;
+var port = 3010;
 
 app.set('view engine', 'hbs');
 
 //use static file - but we don't have any of these. We will be using react.js for the UI and only the express project for API calls
 //app.use(express.static(__dirname + '/public'));
 
+app.use(verifyToken);
+
 app.get('/', (req,res) => {
     res.send(
         { name : "JAKE" }
     );
 });
-
 
 app.listen(port);
 
