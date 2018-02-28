d9746f0b4c9bd153f024a7432a334d6c333556ab
mesync <mesync@gmail.com>
2018-02-28 16:28:36 -0500

    Updating repo

--- server/index.js
+++ server/index.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const app = express();
+const graphqlHTTP = require('express-graphql');
 
-app.get('/',(req,res) => {
-    res.send({ hi: 'there' });
-});
 
 app.listen(3010);
+
+var { graphql, buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    test: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  test: () => {
+      return "this is jakes test";
+  }
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+//graphql(schema, '{ hello }', root).then((response) => {
+ // console.log(response);
+//});
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
