dbdd773e8262401bf5b36d8be9ac8b3dec84a6e8
mesync <mesync@gmail.com>
2018-03-03 10:02:03 -0500

    Adding outerspace background

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -8,13 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-         <Greeting name="Jake" />Jltm1-b-1-1
-        </p>
+        <div class="stars"></div>
+        <div class="twinkling"></div> 
+        <div class="clouds"></div>
       </div>
     );
   }
